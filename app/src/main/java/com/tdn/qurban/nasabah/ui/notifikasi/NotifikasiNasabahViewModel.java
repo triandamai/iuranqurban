3eacdd57a9c051c91971780c7e3de551fe992251
Trian Damai <triannurizkillah@gmail.com>
2020-06-05 19:11:57 +0700

    tambah halaman

---
+++ app/src/main/java/com/tdn/qurban/nasabah/ui/notifikasi/NotifikasiNasabahViewModel.java
@@ -0,0 +1,7 @@
+package com.tdn.qurban.nasabah.ui.notifikasi;
+
+import androidx.lifecycle.ViewModel;
+
+public class NotifikasiNasabahViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
