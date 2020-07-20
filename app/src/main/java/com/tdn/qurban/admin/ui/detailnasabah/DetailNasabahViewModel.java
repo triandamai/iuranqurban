2270cf3ea999d0beaa4accacc023eaf1db20c13b
Trian Damai <triannurizkillah@gmail.com>
2020-07-20 14:46:34 +0700

    building core layer model,data

--- app/src/main/java/com/tdn/qurban/admin/ui/detailnasabah/DetailNasabahViewModel.java
+++ app/src/main/java/com/tdn/qurban/admin/ui/detailnasabah/DetailNasabahViewModel.java
@@ -1,7 +1,50 @@
 package com.tdn.qurban.admin.ui.detailnasabah;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tdn.data.Const;
+import com.tdn.domain.model.userModel;
 
 public class DetailNasabahViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(Const.BASE_CHILD);
+    private ValueEventListener detailNasabah;
+    private MutableLiveData<userModel> detailUsers;
+
+    public DetailNasabahViewModel(ValueEventListener detailNasabah, MutableLiveData<userModel> detailUsers) {
+        this.detailNasabah = detailNasabah;
+        getDetail();
+    }
+
+    private void getDetail() {
+        detailNasabah = databaseReference.child(Const.CHILD_USER).child("").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        databaseReference.addValueEventListener(detailNasabah);
+    }
+
+    public MutableLiveData<userModel> getDetailUsers() {
+        return detailUsers;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        databaseReference.removeEventListener(detailNasabah);
+    }
 }
--- app/src/main/java/com/tdn/qurban/admin/ui/nasabah/NasabahViewModel.java
+++ app/src/main/java/com/tdn/qurban/admin/ui/nasabah/NasabahViewModel.java
@@ -1,7 +1,45 @@
 package com.tdn.qurban.admin.ui.nasabah;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tdn.data.Const;
 
 public class NasabahViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(Const.BASE_CHILD);
+    private ValueEventListener dataNasabah;
+
+    public NasabahViewModel() {
+        getAllNasabah();
+    }
+
+    private void getAllNasabah() {
+        dataNasabah = databaseReference.child(Const.CHILD_USER)
+                .orderByChild("")
+                .startAt(Const.USER_LEVEL_NASABAH)
+                .endAt(Const.USER_LEVEL_NASABAH).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+        databaseReference.addValueEventListener(dataNasabah);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        databaseReference.removeEventListener(dataNasabah);
+    }
 }
