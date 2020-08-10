6cf427e115305a52d3a103f888119f43a54233cd
Trian Damai <triannurizkillah@gmail.com>
2020-08-11 02:51:30 +0700

    revisi

--- app/src/main/java/com/tdn/qurban/nasabah/ui/pengajuantutpakun/PengajuanTutupAkunFragment.java
+++ app/src/main/java/com/tdn/qurban/nasabah/ui/pengajuantutpakun/PengajuanTutupAkunFragment.java
@@ -17,6 +17,8 @@ import android.view.ViewGroup;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.tdn.qurban.R;
+import com.tdn.qurban.core.ActionListener;
+import com.tdn.qurban.core.VMFactory;
 import com.tdn.qurban.databinding.PengajuanTutupAkunFragmentBinding;
 
 public class PengajuanTutupAkunFragment extends Fragment {
@@ -32,7 +34,22 @@ public class PengajuanTutupAkunFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout.pengajuan_tutup_akun_fragment, container, false);
-        mViewModel = new ViewModelProvider(this).get(PengajuanTutupAkunViewModel.class);
+        mViewModel = new ViewModelProvider(this, new VMFactory(getContext(), new ActionListener() {
+            @Override
+            public void onStart() {
+                Snackbar.make(binding.getRoot(), "Proses..", BaseTransientBottomBar.LENGTH_INDEFINITE).show();
+            }
+
+            @Override
+            public void onSuccess(@NonNull String message) {
+                Snackbar.make(binding.getRoot(), message, BaseTransientBottomBar.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(@NonNull String message) {
+                Snackbar.make(binding.getRoot(), message, BaseTransientBottomBar.LENGTH_LONG).show();
+            }
+        })).get(PengajuanTutupAkunViewModel.class);
         onClick();
         return binding.getRoot();
     }
