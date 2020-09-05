228a52948e801990e9c94162d90103a9e0a7e754
triandamai <triannurizkillah@gmail.com>
2020-09-05 09:50:49 +0700

    i am working on it hold on - Working  On pengajuan admin

--- app/src/main/java/com/tdn/qurban/admin/ui/pengajuan/AdapterPenarikanDana.java
+++ app/src/main/java/com/tdn/qurban/admin/ui/pengajuan/AdapterPenarikanDana.java
@@ -10,6 +10,13 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tdn.data.Const;
 import com.tdn.domain.model.TarikDanaModel;
 import com.tdn.qurban.R;
 import com.tdn.qurban.core.AdapterClicked;
@@ -23,6 +30,8 @@ public class AdapterPenarikanDana extends RecyclerView.Adapter<AdapterPenarikanD
     private List<TarikDanaModel> tarikDanaModels = new ArrayList<>();
     private AdapterClicked adapterClicked;
     private Context context;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     public AdapterPenarikanDana(AdapterClicked adapterClicked, Context context) {
         this.adapterClicked = adapterClicked;
@@ -43,7 +52,29 @@ public class AdapterPenarikanDana extends RecyclerView.Adapter<AdapterPenarikanD
         holder.binding.setAction(adapterClicked);
         holder.binding.tvNama.setText(tarikDanaModels.get(position).getAtas_nama());
 
-        holder.binding.tvStatus.setText("Status");
+
+        if (tarikDanaModels.get(position).getAdmin_acc().equalsIgnoreCase(Const.STATUS_NOTIF_PENGAJUANTARIKDANA_DITERIMA)) {
+            holder.binding.tvStatus.setText("Telah Dikonfirmasi");
+        } else if (tarikDanaModels.get(position).getAdmin_acc().equalsIgnoreCase(Const.STATUS_NOTIF_PENGAJUANTARIKDANA_DITOLAK)) {
+            holder.binding.tvStatus.setText("Telah Ditolak");
+        } else if (tarikDanaModels.get(position).getAdmin_acc().equalsIgnoreCase(Const.STATUS_NOTIF_PENGAJUANTARIKDANA_MENUNGGU)) {
+            holder.binding.tvStatus.setText("Menunggu konfirmasi");
+        }
+        databaseReference
+                .child(Const.BASE_CHILD)
+                .child(Const.CHILD_USER)
+                .child(tarikDanaModels.get(position).getUser_uid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     public void setData(List<TarikDanaModel> NotifikasiModels) {
--- app/src/main/java/com/tdn/qurban/admin/ui/pengajuan/AdapterPenutupanAkun.java
+++ app/src/main/java/com/tdn/qurban/admin/ui/pengajuan/AdapterPenutupanAkun.java
@@ -10,7 +10,15 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tdn.data.Const;
 import com.tdn.domain.model.TutupAkunModel;
+import com.tdn.domain.model.UserModel;
 import com.tdn.qurban.R;
 import com.tdn.qurban.core.AdapterClicked;
 import com.tdn.qurban.databinding.ItemTutupakunBinding;
@@ -23,6 +31,8 @@ public class AdapterPenutupanAkun extends RecyclerView.Adapter<AdapterPenutupanA
     private List<TutupAkunModel> tutupAkunModels = new ArrayList<>();
     private AdapterClicked adapterClicked;
     private Context context;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     public AdapterPenutupanAkun(AdapterClicked adapterClicked, Context context) {
         this.adapterClicked = adapterClicked;
@@ -40,9 +50,37 @@ public class AdapterPenutupanAkun extends RecyclerView.Adapter<AdapterPenutupanA
     public void onBindViewHolder(@NonNull AdapterPenutupanAkun.MyViewHolder holder, int position) {
         holder.binding.setAction(adapterClicked);
         holder.binding.setPosisi(position);
-        holder.binding.tvNama.setText("");
-        holder.binding.tvDetail.setText("Mengajuan Penutupan Akun");
-        holder.binding.tvStatus.setText("");
+
+        holder.binding.tvDetail.setText("Mengajukan Penutupan Akun");
+        if (tutupAkunModels.get(position).getAdmin_acc().equalsIgnoreCase(Const.STATUS_NOTIF_TUTUP_DITERIMA)) {
+            holder.binding.tvStatus.setText("Telah Dikonfirmasi");
+        } else if (tutupAkunModels.get(position).getAdmin_acc().equalsIgnoreCase(Const.STATUS_NOTIF_TUTUP_DITOLAK)) {
+            holder.binding.tvStatus.setText("Telah Ditolak");
+        } else if (tutupAkunModels.get(position).getAdmin_acc().equalsIgnoreCase(Const.STATUS_NOTIF_TUTUP_MENUNGGU)) {
+            holder.binding.tvStatus.setText("Menunggu konfirmasi");
+        }
+        databaseReference
+                .child(Const.BASE_CHILD)
+                .child(Const.CHILD_USER)
+                .child(tutupAkunModels.get(position).getUser_uid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            UserModel u = snapshot.getValue(UserModel.class);
+                            u.setUid(snapshot.getKey());
+                            holder.binding.tvNama.setText(u.getNama());
+                        } else {
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
     }
 
     public void setData(List<TutupAkunModel> NotifikasiModels) {
